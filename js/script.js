4bbff26cda21b53a15429c5958746f2dc3b07233
nitinsendhav <nthakur8435@gmail.com>
2025-08-03 00:53:27 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
